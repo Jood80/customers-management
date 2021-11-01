9d0c257da6064cdb36bd47f7ab62bdb02901d7ae
Nujood <shawar.nujood@gmail.com>
2021-11-01 09:56:15 +0200

    Feat(*): display the retrieved data

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,12 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ApiService } from '../shared/api.service';
+import { PersonDTO } from './cards.model';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardsComponent implements OnInit {
-  constructor() {}
+  persons: PersonDTO = new PersonDTO();
+  people: PersonDTO[] = [];
 
-  ngOnInit(): void {}
+  constructor(private api: ApiService) {}
+
+  ngOnInit(): void {
+    this.api.getData().subscribe(
+      (res: any) => (this.people = res),
+      (err: Error) => console.log(err),
+      () => console.log(this.people)
+    );
+  }
 }
